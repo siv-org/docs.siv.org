639f9b2ee304434aedc622e5a204142d873067c8
Ariana Ivan <hi@arianaivan.com>
2023-01-16 14:18:17 -0800

    Add Section 30 Seconds Overview and the illustration

---
+++ content/30sec-overview/index.md
@@ -0,0 +1,8 @@
+---
+part: 30 Seconds Overview
+title: 30 Seconds Overview of a SIV Election
+---
+
+<div class="bg-gray-50">
+![](/images/30sec-overview.png)
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}'
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './content/**/*.md'
   ],
   darkMode: 'class', // remove for 'media'
   theme: {
