a272f8dc9c885ebd0206e5bf7384a5e66f0517e3
Ariana Ivan <hi@arianaivan.com>
2023-02-01 15:46:42 -0800

    All menu items are open by default

--- components/sidebar-section.jsx
+++ components/sidebar-section.jsx
@@ -4,7 +4,7 @@ import SideBarItem from './sidebar-item'
 import { AngleDown, AngleRight } from './svg-icons'
 
 const SideBarSection = ({ toc }) => {
-  const [menuVisible, setMenuVisible] = useState(false)
+  const [menuVisible, setMenuVisible] = useState(true)
   const { asPath: path } = useRouter()
   const chapterItems = (
     <>
@@ -30,9 +30,9 @@ const SideBarSection = ({ toc }) => {
     <div className='text-sm'>
       {/* display toggleable titlebar only when we have a part */}
       {toc.part && (
-        <div className='flex my-3 ml-2 items-center' onClick={toggleMenu}>
+        <div className='flex items-center my-3 ml-2' onClick={toggleMenu}>
           <div>{menuVisible ? <AngleDown /> : <AngleRight />}</div>
-          <div className='pl-1 text-base text-gray-900  dark:text-gray-50 cursor-pointer select-none'>
+          <div className='pl-1 text-base text-gray-900 cursor-pointer select-none dark:text-gray-50'>
             {toc.part}
           </div>
         </div>
