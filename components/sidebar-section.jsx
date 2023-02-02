ce6cd75d94cdcb07561574ac520ae9037f6b0537
Ariana Ivan <hi@arianaivan.com>
2023-02-01 16:07:33 -0800

    ToC arrow icons aligned

--- components/sidebar-section.jsx
+++ components/sidebar-section.jsx
@@ -30,7 +30,7 @@ const SideBarSection = ({ toc }) => {
     <div className='text-sm'>
       {/* display toggleable titlebar only when we have a part */}
       {toc.part && (
-        <div className='flex items-center my-3 ml-2' onClick={toggleMenu}>
+        <div className='flex items-center my-3 ml-1.5' onClick={toggleMenu}>
           <div>{menuVisible ? <AngleDown /> : <AngleRight />}</div>
           <div className='pl-1 text-base text-gray-900 cursor-pointer select-none dark:text-gray-50'>
             {toc.part}
