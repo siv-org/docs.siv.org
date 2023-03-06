da24b4697ef8275f1f3ed2ca0aec103b4dae5638
Ariana Ivan <hi@arianaivan.com>
2023-03-06 15:03:09 -0800

    Cleanup /weaknesses rendering

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,17 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>SIV</span>,
   project: {
-    link: 'https://github.com/dsernst/siv-book',
+    link: 'https://github.com/dsernst/siv-book'
   },
   docsRepositoryBase: 'https://github.com/dsernst/siv-book',
   footer: {
-    text: 'Secure Internet Voting Inc.',
+    text: 'Secure Internet Voting Inc.'
   },
+  components: {
+    ol: (props) => (
+      <ol className='nx-list-decimal ltr:nx-ml-6 rtl:nx-mr-6' {...props} />
+    )
+  }
 }
 
 export default config
