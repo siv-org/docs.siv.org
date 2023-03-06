520b60886678c591517df4a28f8521aef74b338a
Ariana Ivan <hi@arianaivan.com>
2023-03-06 13:15:49 -0800

    Replace template theme links to point to SIV links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SIV</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dsernst/siv-book',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dsernst/siv-book',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Secure Internet Voting Inc.',
   },
 }
 
