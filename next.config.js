9541c122033fdd48c0c380772fa142b736d55b82
Ariana Ivan <hi@arianaivan.com>
2023-03-06 14:36:08 -0800

    Enable LaTeX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
+  latex: true,
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 })
 
 module.exports = withNextra()
