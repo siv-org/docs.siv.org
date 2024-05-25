0c3b5e688670c1be34a68c56504cacaec682f103
David Ernst <git@dsernst.com>
2024-05-24 21:28:23 -0700

    U Subscribe: Better support for dark mode

--- components/UkraineSignup.tsx
+++ components/UkraineSignup.tsx
@@ -7,7 +7,7 @@ export const UkraineSignup = () => {
     <div>
       <label>Get updates:</label>
       <input
-        className='px-2 py-1 mx-2 border rounded border-black/30'
+        className='px-2 py-1 mx-2 border rounded border-black/30 dark:border-white/10'
         placeholder='you@email.com'
         type='email'
         ref={$input}
@@ -16,8 +16,10 @@ export const UkraineSignup = () => {
         }}
       />
       <button
-        className={`px-2 py-1 text-sm border rounded-lg border-black/40 ${
-          buttonText === 'Submit' ? 'active:bg-black/10' : 'cursor-not-allowed '
+        className={`px-2 py-1 text-sm border rounded-lg border-black/40 dark:border-white/20 ${
+          buttonText === 'Submit'
+            ? 'active:bg-black/10 dark:active:bg-white/10'
+            : 'cursor-not-allowed '
         }`}
         disabled={buttonText !== 'Submit'}
         onClick={async () => {
